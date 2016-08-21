92cdb630a402bc5b47df01b8d87c1d7812c499b4
GregorStocks <gregor@gregorstocks.com>
2016-08-21 14:07:58 -0700

    Pretty good image output

--- src/shirt/renderer.clj
+++ src/shirt/renderer.clj
@@ -1,13 +1,28 @@
 (ns shirt.renderer
   (:import [java.awt Color Font])
-  (:require [mikera.image.core :as i]))
+  (:require [mikera.image.core :as i]
+            [clojure.string :as string]))
+
+(def max-line-length 20)
+(defn split-long-lines [lines]
+  (let [result (map #(apply str %)
+                    (mapcat (partial partition-all max-line-length) lines))]
+    (println "splut" lines result)
+    result))
 
 (defn render-shirt-to-image [s]
   (let [image (i/load-image-resource "shirt.jpg")
-        g (i/graphics image)]
+        g (i/graphics image)
+        top-y 150
+        bottom-y 450
+        lines (split-long-lines (string/split s #"\n"))
+        line-height (long (/ (- bottom-y top-y) (count lines)))]
     (.setColor g Color/BLACK)
-    (.setFont g (Font. "Impact" Font/BOLD 30))
-    (.drawString g s 10 25)
+    (.setFont g (Font. "Impact" Font/BOLD line-height))
+    (doall (map (fn [line offset]
+                  (.drawString g line 100
+                               (+ top-y (* offset line-height))))
+                lines (range)))
     (i/show image :title "nice")))
 
 (defn render [config s]
