22f9aa1f7a95960df92fc9228dcc7e5ad5d390dd
GregorStocks <gregor@gregorstocks.com>
2017-01-24 20:53:51 -0800

    lose the leading space

--- src/shirt/partition.clj
+++ src/shirt/partition.clj
@@ -1,11 +1,14 @@
-(ns shirt.partition)
+(ns shirt.partition
+  (:require [clojure.string :as string]))
 
 (defn randomly-partition-string [s]
   (let [partition-offsets (filter #(= (nth s %) \ ) (range (count s)))
         num-partitions (rand-int (count partition-offsets))
         used-offsets (sort (cons 0 (take (dec num-partitions)
                                          (shuffle partition-offsets))))
-        partitions (map (fn [start end] (apply str (take (- end start) (drop start s))))
+        partitions (map (fn [start end] (string/trim
+                                         (apply str
+                                                (take (- end start) (drop start s)))))
                         used-offsets
                         (conj (apply vector (drop 1 used-offsets))
                               (count s)))]
